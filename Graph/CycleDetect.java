922fb095a7e5b8e939630ffe035eb197c8cb0faa
dexterhans <deepakranganathan317@gmail.com>
2017-04-03 21:45:33 +0530

    application of DFS

---
+++ Graph/CycleDetect.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+import java.util.Scanner;
+
+
+
+class Graph{
+        private Integer vertices;
+        private ArrayList<LinkedList<Integer>> adjList;
+        Graph(int n)
+        {
+                this.vertices=n;
+                adjList=new ArrayList<LinkedList<Integer>>();
+                for(int i=0;i<=vertices;i++)
+                        adjList.add(i,new LinkedList<Integer>());// just like initializing with new Integer() we do this. 
+
+        }
+        void addEdge(int v,int w)
+        {
+                adjList.get(v).add(w);
+        }
+        int DFS(int start,int[] visited)
+        {
+                visited[start]=1;
+                //System.out.println(start);
+                Iterator<Integer> iterator=adjList.get(start).iterator();
+                while(iterator.hasNext())
+                {
+                        int x=iterator.next();
+                        if(visited[x]==0)
+                                return DFS(x,visited);
+                        else if(visited[x]==1)
+                        {
+                        	//System.out.println("cycle detected");
+                        	return 1;
+                        }
+                }
+                visited[start]=2;
+                return 0;
+        }
+}
+class CycleDetect{
+
+        public static void main(String[] args)
+        {
+
+                Scanner sc=new Scanner(System.in);
+                int n=sc.nextInt();
+                Graph g=new Graph(n);
+                g.addEdge(1,3);
+                g.addEdge(1,2);
+                g.addEdge(1,5);
+                g.addEdge(5,4);
+                g.addEdge(4,3);
+                g.addEdge(3,1);
+                int[] visited=new int[n+1];
+                int result=g.DFS(1,visited);
+                if(result==0)
+                	System.out.println("cycle not detected");
+                else
+                	System.out.println("cycle detected");
+        }
+}
+
---
+++ Graph/TopologicalSort.java
@@ -0,0 +1,65 @@
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+import java.util.Scanner;
+
+
+
+class Graph{
+        private Integer vertices;
+        //private int time=0;
+        private ArrayList<LinkedList<Integer>> adjList;
+        Graph(int n)
+        {
+                this.vertices=n;
+                adjList=new ArrayList<LinkedList<Integer>>();
+                for(int i=0;i<=vertices;i++)
+                        adjList.add(i,new LinkedList<Integer>());// just like initializing with new Integer() we do this. 
+
+        }
+        void addEdge(int v,int w)
+        {
+                adjList.get(v).add(w);
+        }
+        void DFS(int start,boolean[] visited,LinkedList<Integer> list)
+        {
+                visited[start]=true;
+               // System.out.println(start);
+                Iterator<Integer> iterator=adjList.get(start).iterator();
+                while(iterator.hasNext())
+                {
+                        int x=iterator.next();
+                        if(visited[x]==false)
+                                DFS(x,visited,list);
+                }
+                list.addFirst(start);
+        }
+}
+class TopologicalSort{
+
+        public static void main(String[] args)
+        {
+
+                Scanner sc=new Scanner(System.in);
+                int n=sc.nextInt();
+                Graph g=new Graph(n);
+                g.addEdge(1,3);
+                g.addEdge(1,2);
+                g.addEdge(1,5);
+                g.addEdge(2,4);
+                g.addEdge(4,3);
+                g.addEdge(3,5);
+                boolean[] visited=new boolean[n+1];
+                LinkedList<Integer> list=new LinkedList<Integer>();
+                
+                g.DFS(1,visited,list);
+                Iterator<Integer> iterator=list.iterator();
+                while(iterator.hasNext())
+                {
+                	System.out.print(iterator.next()+" ");
+                }
+        }
+}
+
